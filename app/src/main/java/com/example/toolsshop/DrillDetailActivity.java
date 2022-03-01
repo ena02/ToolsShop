d544be30d821163fefc2bfbdee6c71d81b5031db
ena02 <erkembekov.nurzhan@mail.ru>
2022-03-01 17:56:43 +0600

    i added DrillDetailActivity

--- app/src/main/java/com/example/toolsshop/DrillDetailActivity.java
+++ app/src/main/java/com/example/toolsshop/DrillDetailActivity.java
@@ -1,14 +1,48 @@
 package com.example.toolsshop;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class DrillDetailActivity extends AppCompatActivity {
+
+    private ImageView imageViewDrill;
+    private TextView textViewTitle;
+    private TextView textViewInfo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drill_detail);
+
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        imageViewDrill = findViewById(R.id.imageViewDrillIm);
+        textViewTitle = findViewById(R.id.textViewTitle);
+        textViewInfo = findViewById(R.id.textViewInfo);
+
+        Intent intent = getIntent();
+
+        if (intent.hasExtra("title") && intent.hasExtra("info") && intent.hasExtra("resId")) {
+            int resId = intent.getIntExtra("resId", 0);
+            imageViewDrill.setImageResource(resId);
+            textViewTitle.setText(intent.getStringExtra("title"));
+            textViewInfo.setText(intent.getStringExtra("info"));
+        } else {
+            Intent backToCategory = new Intent(this, DrillCategoryActivity.class);
+            startActivity(backToCategory);
+        }
+
+
     }
 }
